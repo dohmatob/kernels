2478c675a37db56ebf97dd5ad232ac9387dd1eda
elvis <gmdopp@gmail.com>
2012-08-30 12:34:35 +0200

    python/trie.py: added support for graph diplay (ascii art, yes)

---
+++ python/digits_example.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+'''
+:Module: digits_example
+:Synopsis: Classification of hand-written digits usng SVM with mismatch string kernel
+:Author: DOHMATOB Elvis Dopgima
+'''
+
+import sys
+from trie import *
+from sklearn import datasets
+from numpy.random import shuffle
+from numpy import savetxt, nonzero
+from sklearn import svm
+
+
+if __name__ == '__main__':
+    print "\r\n\t\t --[ %s (C)  DOHMATOB Elvis Dopgima ]--\r\n"%sys.argv[0]
+
+    # some useful settings (algorithmic complexity is O(nsamples*(dk)^m))
+    d = 16 # alphabel size
+    k = 6  # branching degree
+    m = 2  # mismatch tolerance
+    nsamples = 500
+
+    # load hand-written digits dataset
+    digits = datasets.load_digits()
+    
+    # prepare training samples
+    X = digits.data[:nsamples]
+    Y = digits.target[:nsamples]
+    
+    # instantial Trie object
+    trie = Trie()
+
+    # use Trie object to compute mismatch string kernel for classifier
+    print
+    print "Computing mismatch string kernel (this may take a while) .." 
+    kernel =  trie.compute_kernel(k, d, training_data=X, m=m)
+    print
+    print "Done."
+    print "Kernel:\r\n", kernel
+    print
+
+    # construct SVM classifier with our mismatch string kernel
+    print "Constructing SVM classifier with our mismatch string kernel .."
+    svc = svm.SVC(kernel='precomputed')
+    print 'Done.'
+    print 
+
+    # fit
+    print "Fitting against training data .."
+    svc.fit(kernel, Y)
+    print "Done (fitting accuracy: %.2f"%(len(nonzero(svc.predict(kernel) == Y)[0])*100.00/nsamples) + "%)."
+    print 
+
--- python/trie.py
+++ python/trie.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 
 """
-:Module: trie.py
-:Synopsis: A quick-and-dirty mplementations of the calculus of "mistmatch string kernels"
+:Module: trie
+:Synopsis: A quick-and-dirty implementation of the calculus of "mistmatch string kernels"
 :Author: DOHMATOB Elvis Dopgima
 """
 
 import unittest
 from numpy import array, zeros, nonzero, all, meshgrid, outer
 
+_alphabet = [chr(x) for x in xrange(ord('A'), ord('Z'))]
+
 class Trie:
     def __init__(self, label=-1, parent=0):
         self._label = label
         self._meta = {}
-        self._rootpath = []
+        self._rootpath = ""
         self._children = dict()
         self.set_parent(parent)
+        self._nodecount = 0
 
         if not self.is_root():
-            self._rootpath.append(label);        
+            self._rootpath += _alphabet[label]
     
     def is_root(self):
         return (self._parent == 0)
@@ -32,49 +35,54 @@ class Trie:
                 self._meta[index] = array([(start, start, 0) for start in starts])
         else:
             meta = dict(self._meta)
-            for index, jmers in meta.iteritems():
+            for index, lmers in meta.iteritems():
                 training_data[index] = training_data[index]
                 # check against length overflow
-                valid = nonzero(jmers[...,1] < len(training_data[index]))[0]
+                valid = nonzero(lmers[:,1] < len(training_data[index]))[0]
                 if len(valid) == 0:
                     del self._meta[index]
                     continue
-                jmers = jmers[valid,...]
+                lmers = lmers[valid,:]
 
-                # update the mismatches of the jmers in this source string
-                mistmatched_jmers = nonzero(training_data[index][jmers[...,1]] != self._label)[0]
-                jmers[mistmatched_jmers,2] += 1
+                # update the mismatches of the lmers in this source string
+                mistmatched_lmers = nonzero(training_data[index][lmers[:,1]] != self._label)[0]
+                lmers[mistmatched_lmers,2] += 1
 
-                # increment of length of all the jmers
-                jmers[...,1] += 1
+                # increment of length of all the lmers
+                lmers[:,1] += 1
 
-                # kill all jmers which have mismatches above the threshold, m
-                valid = nonzero(jmers[...,2] <= m)[0]
+                # kill all lmers which have mismatches above the threshold, m
+                valid = nonzero(lmers[:,2] <= m)[0]
                 if len(valid) == 0:
                     del self._meta[index]
                     continue
-                jmers = jmers[valid,...]
+                lmers = lmers[valid,:]
 
                 # update node meta data
-                self._meta[index] = jmers
+                self._meta[index] = lmers
 
         return len(self._meta) > 0 
         
-    def expand(self, k, d, training_data, m, kernel):
-        nodecount = 1 # number of nodes in the trie
+    def expand(self, k, d, training_data, m, kernel, padding=" "):
         rootpathcount = 0 # number of survived feature-kmers
 
         # inspect node and update its meta data
         go_ahead = self.inspect(training_data, m)
 
+        if self.is_root():
+            print "root\r\n \\"
+        else:
+            print padding[:-1] + '+-' + self._rootpath + '/'
+        padding += ' '
+
         # does this node survive ?
         if go_ahead:
             # is this a surving feature k-mer ?
-            if d == 0:
-                print "".join([str(x) for x in self._rootpath])   
+            if k == 0:
+                # print "".join([_alphabet[x] for x in self._rootpath])   
 
                 # compute the source weights of the training sequences
-                source_weights = array([len(jmers) for jmers in self._meta.values()])
+                source_weights = array([len(lmers) for lmers in self._meta.values()])
                 
                 # compute the contributions of this feature k-mer to the kernel
                 contributions = outer(source_weights, source_weights)
@@ -89,29 +97,39 @@ class Trie:
                     _ = Trie(j, self) 
 
                     # expand child
-                    ga, nc, rc = self._children[j].expand(k, d - 1, training_data, m, kernel)
-                    if not ga:
-                        del self._children[j]
+                    child_padding = padding
+                    if j + 1 == k:
+                        child_padding += ' '
                     else:
-                        # update the counts for this node
-                        nodecount += nc
-                        rootpathcount += rc
+                        child_padding += '|'
+                    ga, rc = self._children[j].expand(k-1, d, training_data, m, kernel, padding=child_padding)                        
 
-        return go_ahead, nodecount, rootpathcount
+                    # update the counts for this node
+                    rootpathcount += rc
+                        
+        return go_ahead, rootpathcount
 
     def is_leaf(self):
         return self.get_nchildren() == 0
 
-    def do_leafs(self, k, callback=None):
-        if  k == 0:
-            if self.is_leaf():
-                if not callback is None:
-                    callback(self)
-                else:
-                    print "".join([str(x) for x in self._rootpath])                
+    def do_leafs(self, padding=' '):
+        if self.is_root():
+            print "root\r\n \\"            
         else:
-            for child in self._children.values():
-                child.do_leafs(k - 1, callback)
+            print padding[:-1] + '+-' + self._rootpath + '/'
+        padding += ' '
+
+        count = 0
+        for _, child in self.get_children().iteritems():
+            count += 1
+            print padding + '|'
+            if not child.is_leaf():
+                if count == self.get_nchildren():
+                    child.do_leafs(padding=padding + ' ')
+                else:
+                    child.do_leafs(padding=padding + '|')                    
+            else:
+                print padding + '+-' + child.get_rootpath()
 
     def compute_kernel(self, k, d, training_data, m=0):
         n = len(training_data)
@@ -120,7 +138,8 @@ class Trie:
         
         # expand trie, constrainted by the training data, and update kernel along the way
         self.expand(k, d, training_data=training_data, m=m, kernel=kernel)
-        
+        # self.do_leafs()
+
         return kernel
 
     def set_parent(self, parent):
@@ -133,18 +152,22 @@ class Trie:
 
     def add_child(self, child):
         self._children[child.get_label()] = child
-        
+        self._nodecount += 1
+    
+    def get_nodecount(self):
+        return self._nodecount
+
     def get_label(self):
         return self._label
 
     def get_rootpath(self):
-        return list(self._rootpath)
+        return self._rootpath
 
     def get_meta(self):
         return dict(self._meta)
 
     def get_children(self):
-        return list(self._children)
+        return dict(self._children)
 
     def get_nchildren(self):
         return len(self._children)
@@ -158,13 +181,10 @@ class TestTrie(unittest.TestCase):
         trie = Trie()
         self.assertEqual(trie.get_label(), -1)
 
-    def test_expand_tree(self):
+    def test_compute_kernel(self):
         trie = Trie()
-        k = 6
-        d = 2
-        # nodecount, rootpathcount = trie.expand(d, k);
-        # self.assertEqual(nodecount, (d**(k+1)-1)/(d-1))
-        # self.assertEqual(rootpathcount, d**k)
+        k = 2
+        d = 7
         training_data = []
         training_data.append([0,1,1,1,0,1,0,1,1,0,0,0,1])
         training_data.append([1,1,0,1,0,1,0,0,1,0,0,0,0])
@@ -173,10 +193,12 @@ class TestTrie(unittest.TestCase):
         training_data.append([1,1,0,1,0,1,0,0,1,0,0,0,0])
         training_data.append([0,1,0,1,0,1,0,0,1,0,0,0,1])
         training_data.append([1,1,0,1,0,1,0,0,1,0,0,0,1])
+        
 
         print trie.compute_kernel(d, k, training_data=training_data, m=1)
-        for seq in training_data:
-            print "".join([str(x) for x in seq])
+
+        # for seq in training_data:
+        #     print "".join([str(x) for x in seq])
 
 if __name__ == '__main__':
     unittest.main()
